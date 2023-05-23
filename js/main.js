e78b817d82e89409ab4560ce8471061afc51c71d
zero-penguin <arato1060@outlook.jp>
2023-05-23 12:22:36 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const app = new Vue({ 
+  el: '#app', 
+  data: { 
+    message: 'Hello Vue!',
+    items: [
+      {id:1,num:"a"},
+      {id:2,num:"b"},
+      {id:3,num:"c"},
+      {id:4,num:"d"}
+    ],
+
+    additem:"",
+
+    seen: true
+  },
+  methods:{
+    Additem: function () {
+      this.items.push(
+        {id:this.index++, num: this.additem}
+        );
+    }
+  }
+})
